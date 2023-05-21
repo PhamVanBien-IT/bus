85a35b603cd1d70b3e433867a063f5094758dec4
PhamVanBien-IT <pvb0204@gmail.com>
2023-05-21 22:44:45 +0700

    eventBus

---
+++ src/components/TheWelcome.vue
@@ -0,0 +1,19 @@
+<template>
+  <button value="click" @click="btnBus"></button>
+</template>
+<script>
+import eventBus from '../store/eventBus';
+export default{
+  name:"TheWelcome",
+ methods:{
+  btnBus(){
+    eventBus.emit('test', "ok");
+  },
+  data(){
+    return{
+      data : "Ok"
+    }
+  }
+ }
+}
+</script>
---
+++ src/store/eventBus.js
@@ -0,0 +1,27 @@
+const eventBus = {
+  events: {},
+  on(event, callback) {
+    if (!this.events[event]) {
+      this.events[event] = []
+    }
+    this.events[event].push(callback)
+  },
+  emit(event, data) {
+    if (this.events[event]) {
+      this.events[event].forEach(callback => {
+        callback(data)
+      })
+    }
+  },
+  off(event, callback) {
+    if (this.events[event]) {
+      if (callback) {
+        this.events[event] = this.events[event].filter(cb => cb !== callback)
+      } else {
+        delete this.events[event]
+      }
+    }
+  }
+}
+
+export default eventBus
